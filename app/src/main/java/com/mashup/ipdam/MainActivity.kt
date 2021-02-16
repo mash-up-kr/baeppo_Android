fcb4552b0786d14d1a8394e7833df9d45563d5fc
jaeryo2357 <jaeryo2357@naver.com>
2021-02-16 11:27:24 +0900

    feat: base module

--- app/src/main/java/com/mashup/ipdam/MainActivity.kt
+++ app/src/main/java/com/mashup/ipdam/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.mashup.ipdam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mashup.base.BaseActivity
+import com.mashup.ipdam.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override var logTag: String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
