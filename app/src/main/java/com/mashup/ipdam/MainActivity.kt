208b104e25dae04820c67ce85d915c8b0808974e
jaeryo2357 <jaeryo2357@naver.com>
2021-02-20 03:19:37 +0900

    feat: Map 연결

--- app/src/main/java/com/mashup/ipdam/MainActivity.kt
+++ app/src/main/java/com/mashup/ipdam/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.mashup.ipdam
 
-import android.os.Bundle
 import com.mashup.base.BaseActivity
 import com.mashup.ipdam.databinding.ActivityMainBinding
 
@@ -8,8 +7,4 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
     override var logTag: String = "MainActivity"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
 }
