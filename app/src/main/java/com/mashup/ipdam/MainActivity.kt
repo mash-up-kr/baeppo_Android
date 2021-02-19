42118795ea293e6a60ba698c47ec736dd5d117ca
jaeryo2357 <jaeryo2357@naver.com>
2021-02-20 03:19:37 +0900

    feat: Map 연결

---
+++ app/src/main/java/com/mashup/ipdam/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.mashup.ipdam
+
+import com.mashup.base.BaseActivity
+import com.mashup.ipdam.databinding.ActivityMainBinding
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override var logTag: String = "MainActivity"
+
+}
