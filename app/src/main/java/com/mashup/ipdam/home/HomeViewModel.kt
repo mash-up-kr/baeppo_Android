58a18debee71c70f6a1ae151e83e5395c3fa03cf
jaeryo2357 <jaeryo2357@naver.com>
2021-02-25 16:54:19 +0900

    refactor: Reformat Code

--- app/src/main/java/com/mashup/ipdam/home/HomeViewModel.kt
+++ app/src/main/java/com/mashup/ipdam/home/HomeViewModel.kt
@@ -6,7 +6,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(): BaseViewModel() {
+class HomeViewModel @Inject constructor() : BaseViewModel() {
     override var logTag: String = "HomeViewModel"
 
     fun showIpdamInfo(symbolPosition: LatLng) {
