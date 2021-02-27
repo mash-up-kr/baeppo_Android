03540f3637c00389df1e83eb17c3334e459d4aea
jaeryo2357 <jaeryo2357@naver.com>
2021-02-28 08:03:29 +0900

    refactor: 주소 검색 RepositoryImpl Inject

--- app/src/main/java/com/mashup/ipdam/ui/search/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/com/mashup/ipdam/ui/search/data/repository/SearchRepositoryImpl.kt
@@ -1,8 +1,9 @@
 package com.mashup.ipdam.ui.search.data.repository
 
 import com.mashup.ipdam.network.service.KakaoService
+import javax.inject.Inject
 
-class SearchRepositoryImpl(
+class SearchRepositoryImpl @Inject constructor(
     private val kakaoService: KakaoService
 ) : SearchRepository {
     override fun getPlaceByKeyword(keyword: String) = kakaoService.requestAddressByKeyword(keyword)
