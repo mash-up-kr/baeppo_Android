2b5a91857c310fc55006e9611f7e7c921f2a1e96
jaeryo2357 <jaeryo2357@naver.com>
2021-02-28 10:13:50 +0900

    feat: 카카오 검색 API 적용

--- app/src/main/java/com/mashup/ipdam/network/service/KakaoService.kt
+++ app/src/main/java/com/mashup/ipdam/network/service/KakaoService.kt
@@ -2,12 +2,14 @@ package com.mashup.ipdam.network.service
 
 import com.mashup.ipdam.ui.search.data.PlaceKeyword
 import io.reactivex.Single
-import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface KakaoService {
+    @Headers("Authorization: KakaoAK cebbe69291069180ea7e957572c9998f")
     @GET("v2/local/search/keyword.json")
     fun requestAddressByKeyword(
-        @Field("query") keyword: String,
+        @Query("query") keyword: String,
     ): Single<PlaceKeyword>
 }
