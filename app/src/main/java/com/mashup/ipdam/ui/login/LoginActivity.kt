df430056924b87ead9d5fc2957fae67b7b1b9f4b
jaeryo2357 <jaeryo2357@naver.com>
2021-03-04 00:43:26 +0900

    feat: MainActivity 연결

--- app/src/main/java/com/mashup/ipdam/ui/login/LoginActivity.kt
+++ app/src/main/java/com/mashup/ipdam/ui/login/LoginActivity.kt
@@ -1,13 +1,26 @@
 package com.mashup.ipdam.ui.login
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
 import com.mashup.base.BaseActivity
 import com.mashup.ipdam.R
 import com.mashup.ipdam.databinding.ActivityLoginBinding
+import com.mashup.ipdam.ui.main.MainActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     override var logTag: String = "LoginActivity"
 
-
+    override fun initLayout() {
+        binding.loginButton.setOnClickListener {
+            //TODO: 로그인 로직
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        binding.loginCreateAccount.setOnClickListener {
+            //TODO: 회원가입 로직
+        }
+        binding.loginFindAccount.setOnClickListener {
+            //TODO: 아이디 및 비밀번호 찾끼 로직
+        }
+    }
 }
