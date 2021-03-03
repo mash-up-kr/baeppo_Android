c071057b2869f66f63be107192352517cdd15e6e
jaeryo2357 <jaeryo2357@naver.com>
2021-03-03 23:21:44 +0900

    feat: Login UI 작성

---
+++ app/src/main/java/com/mashup/ipdam/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.mashup.ipdam.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mashup.base.BaseActivity
+import com.mashup.ipdam.R
+import com.mashup.ipdam.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override var logTag: String = "LoginActivity"
+
+
+}
