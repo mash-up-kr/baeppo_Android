62545e1d7ac47ec06ac6dd3127a4764ef8dee9cc
jaeryo2357 <jaeryo2357@naver.com>
2021-03-04 16:56:25 +0900

    feat: App version 표시

--- app/src/main/java/com/mashup/ipdam/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/mashup/ipdam/ui/profile/ProfileFragment.kt
@@ -1,5 +1,7 @@
 package com.mashup.ipdam.ui.profile
 
+import android.content.pm.PackageManager
+import android.util.Log
 import com.mashup.base.BaseFragment
 import com.mashup.ipdam.R
 import com.mashup.ipdam.databinding.FragmentProfileBinding
@@ -9,6 +11,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
 
     override var logTag: String = "ProfileFragment"
+
+    override fun initLayout() {
+        initVersionName()
+    }
+
+    private fun initVersionName() {
+        try {
+            val packageInfo = requireContext().packageManager.getPackageInfo(
+                requireContext().packageName, 0
+            )
+            binding.profileVersionValue.text = packageInfo.versionName
+        } catch (e : PackageManager.NameNotFoundException) {
+            Log.e(logTag, e.message ?: "PackageManagerName Not Found")
+        }
+    }
 
     companion object {
         fun getInstance() = ProfileFragment()
