783cae1a1fb299d981718e53bbcf18a6f5134d69
jaeryo2357 <jaeryo2357@naver.com>
2021-03-28 17:51:27 +0900

    feat: 리뷰 생성 페이지와 관련 벡터 이미지 추가

---
+++ app/src/main/java/com/mashup/ipdam/ui/create/CreateActivity.kt
@@ -0,0 +1,11 @@
+package com.mashup.ipdam.ui.create
+
+import com.mashup.base.BaseActivity
+import com.mashup.ipdam.R
+import com.mashup.ipdam.databinding.ActivityCreateBinding
+
+class CreateActivity : BaseActivity<ActivityCreateBinding>(R.layout.activity_create) {
+    override var logTag: String = "CreateActivity"
+
+
+}
