121d1487f6aa22e3e6687fffaaa3fbba5cc68dbc
jaeryo2357 <jaeryo2357@naver.com>
2021-04-09 01:14:03 +0900

    feat: 회원가입 아이디, 비밀번호 검증하는 로직 추가

--- app/src/main/java/com/mashup/ipdam/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/mashup/ipdam/ui/register/RegisterActivity.kt
@@ -1,27 +1,61 @@
 package com.mashup.ipdam.ui.register
 
 import android.view.View
+import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import com.mashup.base.BaseActivity
-import com.mashup.base.ext.setDecorFitStatusBar
 import com.mashup.ipdam.R
 import com.mashup.ipdam.databinding.ActivityRegisterBinding
 
 class RegisterActivity : BaseActivity<ActivityRegisterBinding>(R.layout.activity_register) {
     override var logTag: String = "RegisterActivity"
+    private val registerViewModel: RegisterViewModel by viewModels()
 
     override fun initLayout() {
-        setDecorFitStatusBar()
-        binding.registerBackButton.setOnClickListener {
-            finish()
+        binding.apply {
+            viewModel = registerViewModel
+            registerBackButton.setOnClickListener {
+                finish()
+            }
+            registerButton.setOnClickListener {
+                //TODO: 회원가입 로직 작성
+            }
         }
+        initSearchEditText()
+    }
 
-        binding.registerButton.setOnClickListener {
-            //TODO: 회원가입 로직 작성
+    private fun initSearchEditText() {
+        binding.registerSearchButton.setOnClickListener {
+            showSearchSchoolView()
         }
     }
 
-    fun showIdCheckSuccessLayout() {
+    override fun observeViewModel() {
+        super.observeViewModel()
+        registerViewModel.idCorrect.observe(this) { isCorrect ->
+            if (isCorrect) {
+                showIdCheckSuccessLayout()
+            } else {
+                showIdCheckFailedLayout()
+            }
+        }
+        registerViewModel.passwordCheckCorrect.observe(this) { isCorrect ->
+            if (isCorrect) {
+                showPasswordCheckSuccessLayout()
+            } else {
+                showPasswordCheckFailedLayout()
+            }
+        }
+        registerViewModel.requestSearchSchool.observe(this) {
+            showSearchSchoolView()
+        }
+    }
+
+    private fun showSearchSchoolView() {
+        //TODO: 학교 검색 화면
+    }
+
+    private fun showIdCheckSuccessLayout() {
         binding.registerIdResult.run {
             visibility = View.VISIBLE
             text = getString(R.string.register_id_result_success)
@@ -29,7 +63,7 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(R.layout.activity
         }
     }
 
-    fun showIdCheckFailedLayout() {
+    private fun showIdCheckFailedLayout() {
         binding.registerIdResult.run {
             visibility = View.VISIBLE
             text = getString(R.string.register_id_result_failed)
@@ -37,7 +71,7 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(R.layout.activity
         }
     }
 
-    fun showPasswordCheckSuccessLayout() {
+    private fun showPasswordCheckSuccessLayout() {
         binding.registerPasswordResult.run {
             visibility = View.VISIBLE
             text = getString(R.string.register_password_result_success)
@@ -45,7 +79,7 @@ class RegisterActivity : BaseActivity<ActivityRegisterBinding>(R.layout.activity
         }
     }
 
-    fun showPasswordCheckFailedLayout() {
+    private fun showPasswordCheckFailedLayout() {
         binding.registerPasswordResult.run {
             visibility = View.VISIBLE
             text = getString(R.string.register_password_result_failed)
---
+++ app/src/main/java/com/mashup/ipdam/ui/register/RegisterViewModel.kt
@@ -0,0 +1,40 @@
+package com.mashup.ipdam.ui.register
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import com.mashup.base.BaseViewModel
+import com.mashup.ipdam.SingleLiveEvent
+
+class RegisterViewModel : BaseViewModel() {
+    override var logTag: String = "RegisterViewModel"
+
+    val id = MutableLiveData("")
+    val password = MutableLiveData("")
+    val passwordCheck = MutableLiveData("")
+    val searchSchool = MutableLiveData("")
+    private val _requestSearchSchool = SingleLiveEvent<Unit>()
+    val requestSearchSchool: SingleLiveEvent<Unit> = _requestSearchSchool
+
+    val idCorrect: LiveData<Boolean> = Transformations.map(id) {
+        isIdCorrect(it)
+    }
+    val passwordCheckCorrect: LiveData<Boolean> = Transformations.map(passwordCheck) {
+        isPasswordCheckCorrect(it)
+    }
+
+    fun showSearchSchoolResult() {
+        _requestSearchSchool.value = Unit
+    }
+
+    private fun isIdCorrect(id: String): Boolean {
+        val idRegex = Regex("[0-9]+[a-zA-z]+")
+        val lengthRegex = Regex("[0-9a-zA-Z]{6,12}")
+        return idRegex.matches(id) && lengthRegex.matches(id)
+    }
+
+    private fun isPasswordCheckCorrect(passwordCheck: String): Boolean {
+        val passwordValue = password.value ?: ""
+        return if (passwordValue.isEmpty()) false else passwordValue == passwordCheck
+    }
+}
